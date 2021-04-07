d576450dc1608668fb77f2681dddd778a1fd0d7d
Basit Ali <basitk41@gmail.com>
2021-04-07 22:20:39 +0500

    Add files via upload

---
+++ src/components/TaskList.test.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import "@testing-library/jest-dom/extend-expect";
+
+// import { WithPinnedTasks } from "./TaskList.stories"; //👈  Our story imported here
+
+// it("renders pinned tasks at the start of the list", () => {
+//   const div = document.createElement("div");
+//   //👇 Story's args used with our test
+//   ReactDOM.render(<WithPinnedTasks {...WithPinnedTasks.args} />, div);
+
+//   // We expect the task titled "Task 6 (pinned)" to be rendered first, not at the end
+//   const lastTaskInput = div.querySelector(
+//     '.list-item:nth-child(1) input[value="Task 6 (pinned)"]'
+//   );
+//   expect(lastTaskInput).not.toBe(null);
+
+//   ReactDOM.unmountComponentAtNode(div);
+// });
